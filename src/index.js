b864b9b99091c91a7a710502bc30dd312ef67f3c
joshuasunil10 <joshuasunilmathew10@gmail.com>
2024-11-20 02:06:46 +0000

    latest update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
-const express = require('express');
+const express = require("express");
 const path = require("path");
 const bcrypt = require("bcrypt");
+const { Client } = require('pg');
 
+// Connect to PostgreSQL database
+
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: '1234',
+    port: 54321,
+});
+
+client.connect()
+    .then(() => console.log('Connected to PostgreSQL database'))
+    .catch(err => console.error('Connection error', err.stack));
+
+
+module.exports = client;
 const app = express();
 app.set('view engine', 'ejs');
 
@@ -23,6 +40,10 @@ app.get("/login", (req, res) => {
 // Define the signup route
 app.get("/register", (req, res) => {
     res.render("register");
+});
+
+app.get("/dashboard", (req, res) => {
+    res.render("dashboard");
 });
 
 const port = 3000;
